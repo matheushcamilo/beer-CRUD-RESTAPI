9547e1a137fbf071a03300c50256130daac94cb7
matheushcamilo <matheushcamilo1997@gmail.com>
2021-10-02 10:41:32 -0300

    First commit

---
+++ src/test/java/com/digitalinnovationone/beerDIO/BeerDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.beerDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
